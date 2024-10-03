674fa386af7984f825e17b59b1acc394b4b4c13b
Samuel Ezra Nathan Andrew Sirait <samuelezra2013@gmail.com>
2024-10-03 22:23:47 +0800

    finish the code but still problem in the port #1

--- src/server.js
+++ src/server.js
@@ -1,21 +1,31 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
- 
-const init = async () => {
-const server = Hapi.server({
-    port: 9000,
-    host: 'localhost',
-    routes: {
-        cors: {
-        origin: ['*'],
-        },
-    },
-});
- 
-server.route(routes);
- 
-await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+
+const config = {
+  port: 9000,
+  host: 'localhost',
 };
- 
-init();
+
+/**
+ * Initiate Hapi Server
+ * @param {Object} c
+ * @param {number} c.port
+ * @param {string} c.host
+ * @returns {Promise<Hapi.Server>}
+ */
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 9000, // Gunakan PORT dari environment variable
+    host: 'localhost',
+  });
+
+  server.route(routes);
+
+  await server.start();
+  // Mengaktifkan console.log untuk melihat apakah server sudah berjalan
+  console.log(`Server berjalan pada ${server.info.uri}`);
+
+  return server;
+};
+
+init(config);
